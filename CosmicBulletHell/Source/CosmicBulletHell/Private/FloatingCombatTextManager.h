5958374b189ed42103c8fb5e8ea350f7a33151e1
Antropy <70490965+Antropic1@users.noreply.github.com>
2023-10-25 17:43:28 -0400

    Reverted prior upload attempt
    Only added the new cpp file and header

--- CosmicBulletHell/Source/CosmicBulletHell/Private/FloatingCombatTextManager.h
+++ CosmicBulletHell/Source/CosmicBulletHell/Private/FloatingCombatTextManager.h
@@ -1,15 +1,69 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Blueprint/UserWidget.h"
+#include "ActorHealthManager.h"
+#include "FloatingCombatTextManager.generated.h"
 
 /**
- * 
+ * A class responsible for managing and displaying floating combat text
+ * when actors take damage, utilizing a pool of reusable widgets for optimized performance.
  */
-class FloatingCombatTextManager
+UCLASS()
+class YOURGAME_API AFloatingCombatTextManager : public AActor
 {
+    GENERATED_BODY()
+
 public:
-	FloatingCombatTextManager();
-	~FloatingCombatTextManager();
+    // Constructor
+    AFloatingCombatTextManager();
+
+    // The font used to render combat text
+    UPROPERTY(BlueprintReadWrite, Category = "Combat Text")
+    UFont* CombatTextFont;
+
+    // The color of the combat text
+    UPROPERTY(BlueprintReadWrite, Category = "Combat Text")
+    FColor CombatTextColor;
+
+    // The duration for which the combat text is displayed
+    UPROPERTY(BlueprintReadWrite, Category = "Combat Text")
+    float CombatTextDuration;
+
+    // The initial velocity of the combat text
+    UPROPERTY(BlueprintReadWrite, Category = "Combat Text")
+    FVector CombatTextVelocity;
+
+    // Displays floating combat text showing the amount of damage taken by the specified actor
+    UFUNCTION(BlueprintCallable, Category = "Combat Text")
+    void DisplayCombatText(AActor* Actor, float DamageAmount);
+
+    // Sets up a reference to the ActorHealthManager to be used for retrieving damage data
+    UFUNCTION(BlueprintCallable, Category = "Combat Text")
+    void SetupHealthManagerReference(AActorHealthManager* HealthManager);
+
+protected:
+    // Reference to the ActorHealthManager to retrieve damage data
+    UPROPERTY()
+    AActorHealthManager* ActorHealthManagerRef;
+
+    // A pool of reusable combat text widgets to optimize performance
+    UPROPERTY()
+    TArray<UUserWidget*> CombatTextPool;
+
+    // Subscribes the FloatingCombatTextManager to the damage events of the ActorHealthManager
+    UFUNCTION()
+    void SubscribeToDamageEvents();
+
+    // Handler function to be called when an actor takes damage, triggering the display of combat text
+    UFUNCTION()
+    void OnActorDamaged(AActor* Actor, float DamageAmount);
+
+    // Returns a combat text widget to the pool for reuse
+    UFUNCTION()
+    void ReturnCombatTextToPool(UUserWidget* CombatTextWidget);
+
+    // Unreal Engine event called at the start of play
+    virtual void BeginPlay() override;
 };
